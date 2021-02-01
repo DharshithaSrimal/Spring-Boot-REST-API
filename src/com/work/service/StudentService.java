819aeaaffee309d157ff45d90ffdf6805baeb6e1
DharshithaSrimal <dharshithasrimal@gmail.com>
2021-02-01 22:51:36 +0530

    Update StudentService.java

--- src/com/work/service/StudentService.java
+++ src/com/work/service/StudentService.java
@@ -1,5 +1,16 @@
-package com.work.service;
+package com.bushansirgur.service;
+
+import java.util.List;
+import com.bushansirgur.entity.Student;
 
 public interface StudentService {
+
+	public void saveStudentObj(Student studentObj);
+
+	public List<Student> getAllStudents();
+
+	public Student getStudentObj(int theId);
+
+	public void removeStudentObj(int theId);
 
 }
